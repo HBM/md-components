1dae3407eb0a70be9f1cd3623979dad86a801ae0
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-07-11 15:53:30 +0200

    progress: fix path to component

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 export Button from './components/button/'
 export Card from './components/card/'
-export {Linear, Circular} from './components/circular-progress/'
+export {Linear, Circular} from './components/progress/'
 export Checkbox from './components/checkbox/'
 export Header from './components/header/'
 export Icon from './components/icon/'
