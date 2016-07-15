47c82f518715ae794be1f2d91a32b5c1895ddff3
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-07-15 12:02:39 +0200

    use only single browser for now until async issues are fixed

--- test/e2e/selenium.js
+++ test/e2e/selenium.js
@@ -26,27 +26,27 @@ var capabilities = [
     platform: 'Linux',
     browserName: 'Chrome',
     version: '48'
-  },
-  {
-    platform: 'Linux',
-    browserName: 'Firefox',
-    version: '45'
-  },
-  {
-    platform: 'Windows 10',
-    browserName: 'Chrome',
-    version: '51'
-  },
-  {
-    platform: 'Windows 10',
-    browserName: 'Firefox',
-    version: '47'
-  },
-  {
-    platform: 'Windows 10',
-    browserName: 'MicrosoftEdge',
-    version: '13.10586'
   }
+  // {
+  //   platform: 'Linux',
+  //   browserName: 'Firefox',
+  //   version: '45'
+  // },
+  // {
+  //   platform: 'Windows 10',
+  //   browserName: 'Chrome',
+  //   version: '51'
+  // },
+  // {
+  //   platform: 'Windows 10',
+  //   browserName: 'Firefox',
+  //   version: '47'
+  // },
+  // {
+  //   platform: 'Windows 10',
+  //   browserName: 'MicrosoftEdge',
+  //   version: '13.10586'
+  // }
   // {
   //   platform: 'Windows 10',
   //   browserName: 'Internet Explorer',
@@ -59,20 +59,21 @@ var capabilities = [
   // }
 ]
 
-var driver
-after(function (done) {
-  driver.quit()
-  // without timeout sauce labs tests do not end properly and
-  // report "Your test errored. Test did not see a new command for 90 seconds. Timing out."
-  setTimeout(function () {
-    done()
-  }, 1000)
-})
-
 capabilities.forEach(function (capability) {
   const description = capability.platform + ': ' + capability.browserName + ' ' + capability.version + ': '
+
   describe(description, function () {
-    var id
+    let id
+    let driver
+
+    after(function (done) {
+      driver.quit()
+      // without timeout sauce labs tests do not end properly and
+      // report "Your test errored. Test did not see a new command for 90 seconds. Timing out."
+      setTimeout(function () {
+        done()
+      }, 1000)
+    })
 
     before(function (done) {
       driver = new webdriver.Builder()
@@ -93,18 +94,18 @@ capabilities.forEach(function (capability) {
       })
     })
 
-    afterEach(function (done) {
-      var title = this.currentTest.fullTitle()
-      var passed = this.currentTest.state === 'passed'
-      saucelabs.updateJob(id, {
-        name: title,
-        passed: passed,
-        // we have to set a build id to make the badges work
-        build: 1
-      }, done)
-    })
-
     describe('Button', function () {
+      afterEach(function (done) {
+        var title = this.currentTest.fullTitle()
+        var passed = this.currentTest.state === 'passed'
+        saucelabs.updateJob(id, {
+          name: title,
+          passed: passed,
+          // we have to set a build id to make the badges work
+          build: 1
+        }, done)
+      })
+
       it('should be clickable', function () {
         driver.get('http://localhost:1337/index.html#/button')
         driver.findElement(By.className('Button')).click()
