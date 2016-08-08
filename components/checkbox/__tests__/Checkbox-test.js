ac81430276b45b05883ef5571b596dca1bf1d71c
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-08-08 10:16:54 +0200

    checkbox: rewrite tests using enzyme

--- components/checkbox/__tests__/Checkbox-test.js
+++ components/checkbox/__tests__/Checkbox-test.js
@@ -1,61 +1,43 @@
 /* global describe, expect, it */
 
 import React from 'react'
+import {shallow} from 'enzyme'
 import Checkbox from '../'
-import {createRenderer} from 'react-addons-test-utils'
 
 describe('Checkbox', () => {
   it('should render a simple example', () => {
-    let renderer = createRenderer()
-    renderer.render(<Checkbox onChange={() => {}} />)
-    let box = renderer.getRenderOutput()
-    expect(box).toBeTruthy()
+    const wrapper = shallow(<Checkbox onChange={() => {}} />)
+    expect(wrapper.find('.Checkbox').length).toBe(1)
   })
 
   it('should not be checked by default', () => {
-    let renderer = createRenderer()
-    renderer.render(<Checkbox onChange={() => {}} />)
-    let box = renderer.getRenderOutput()
-    expect(box.props.children[0].props.checked).toBe(false)
+    const wrapper = shallow(<Checkbox onChange={() => {}} />)
+    expect(wrapper.find('.Checkbox-input').props().checked).toBe(false)
   })
 
   it('should render a checked checkbox', () => {
-    let renderer = createRenderer()
-    renderer.render(<Checkbox checked onChange={() => {}} />)
-    let box = renderer.getRenderOutput()
-    expect(box.props.children[0].props.checked).toBe(true)
+    const wrapper = shallow(<Checkbox checked onChange={() => {}} />)
+    expect(wrapper.find('.Checkbox-input').props().checked).toBe(true)
   })
 
   it('should render a disabled checkbox', () => {
-    let renderer = createRenderer()
-    renderer.render(<Checkbox disabled onChange={() => {}} />)
-    let box = renderer.getRenderOutput()
-    expect(box.props.children[0].props.disabled).toBe(true)
+    const wrapper = shallow(<Checkbox disabled onChange={() => {}} />)
+    expect(wrapper.find('.Checkbox-input').props().disabled).toBe(true)
   })
 
   it('should have a default label', () => {
-    let renderer = createRenderer()
-    renderer.render(<Checkbox onChange={() => {}} />)
-    let box = renderer.getRenderOutput()
-    expect(box.props.children[3].props.children).toBe('Label')
+    const wrapper = shallow(<Checkbox onChange={() => {}} />)
+    expect(wrapper.find('.Checkbox-label').text()).toBe('Label')
   })
 
   it('should render a custom label', () => {
-    let renderer = createRenderer()
-    renderer.render(<Checkbox label='custom label' onChange={() => {}} />)
-    let box = renderer.getRenderOutput()
-    expect(box.props.children[3].props.children).toBe('custom label')
+    const wrapper = shallow(<Checkbox label='custom label' onChange={() => {}} />)
+    expect(wrapper.find('.Checkbox-label').text()).toBe('custom label')
   })
 
-  it('should toggle between true and false when clicked', () => {
-    let renderer = createRenderer()
-    let changed = false
-    let change = () => {
-      changed = true
-    }
-    renderer.render(<Checkbox onChange={change} />)
-    let box = renderer.getRenderOutput()
-    box.props.children[0].props.onChange()
-    expect(changed).toBe(true)
+  it('should callback when clicked', (done) => {
+    const change = () => done()
+    const wrapper = shallow(<Checkbox onChange={change} />)
+    wrapper.find('.Checkbox-input').simulate('change')
   })
 })
