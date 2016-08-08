cd19fe80cf3c1d09d3df947f8f47d0be5990f19a
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-08-08 09:52:14 +0200

    button: rewrite tests using enzyme

--- components/button/__tests__/Button-test.js
+++ components/button/__tests__/Button-test.js
@@ -1,33 +1,23 @@
 /* global describe, it, expect */
 
 import React from 'react'
+import {shallow} from 'enzyme'
 import Button from '../'
-import {createRenderer} from 'react-addons-test-utils'
 
 describe('Button', () => {
-  it('should bubble up click events', () => {
-    let renderer = createRenderer()
-    let clicked = false
-    let click = () => {
-      clicked = true
-    }
-    renderer.render(<Button onClick={click} />)
-    let button = renderer.getRenderOutput()
-    button.props.onClick()
-    expect(clicked).toBe(true)
+  it('should bubble up click events', (done) => {
+    const click = () => done()
+    const wrapper = shallow(<Button onClick={click} />)
+    wrapper.find('.Button').simulate('click')
   })
 
   it('should have `Button--raised` class when raised property is true', () => {
-    let renderer = createRenderer()
-    renderer.render(<Button onClick={() => {}} raised />)
-    let button = renderer.getRenderOutput()
-    expect(button.props.className).toContain('Button--raised')
+    const wrapper = shallow(<Button onclick={() => {}} raised />)
+    expect(wrapper.find('.Button').hasClass('Button--raised')).toBe(true)
   })
 
   it('should be disabled when told so', () => {
-    let renderer = createRenderer()
-    renderer.render(<Button onClick={() => {}} disabled />)
-    let button = renderer.getRenderOutput()
-    expect(button.props.disabled).toBe(true)
+    const wrapper = shallow(<Button onClick={() => {}} disabled />)
+    expect(wrapper.find('button').props().disabled).toBe(true)
   })
 })
