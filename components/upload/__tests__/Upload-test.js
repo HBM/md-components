4ced5585d48f25098e656c7e61fa0955afc6c5ae
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-08-08 16:15:42 +0200

    upload: rewrite tests using enzyme

--- components/upload/__tests__/Upload-test.js
+++ components/upload/__tests__/Upload-test.js
@@ -1,8 +1,7 @@
 /* global describe, it, Event, expect */
 
 import React from 'react'
-import ReactDOM from 'react-dom'
-import TestUtils from 'react-addons-test-utils'
+import {mount} from 'enzyme'
 import Upload from '../'
 
 const file = {
@@ -14,63 +13,46 @@ const noop = () => {}
 
 describe('Upload', () => {
   it('should render component with default text', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const textBefore = TestUtils.findRenderedDOMComponentWithClass(instance, 'Upload-textBefore')
-    expect(textBefore.textContent).toEqual('')
-    const textAfter = TestUtils.findRenderedDOMComponentWithClass(instance, 'Upload-textAfter')
-    expect(textAfter.textContent).toEqual('or drag file in this area')
+    const wrapper = mount(<Upload onChange={noop} />)
+    expect(wrapper.find('.Upload-textBefore').text()).toEqual('')
+    expect(wrapper.find('.Upload-textAfter').text()).toEqual('or drag file in this area')
   })
 
   it('should render component with custom textBefore', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload textBefore={'hello world'} onChange={noop} />)
-    const textBefore = TestUtils.findRenderedDOMComponentWithClass(instance, 'Upload-textBefore')
-    expect(textBefore.textContent).toEqual('hello world')
+    const wrapper = mount(<Upload textBefore={'hello world'} onChange={noop} />)
+    expect(wrapper.find('.Upload-textBefore').text()).toEqual('hello world')
   })
 
   it('should render component with custom textAfter', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload textAfter={'hello world'} onChange={noop} />)
-    const textAfter = TestUtils.findRenderedDOMComponentWithClass(instance, 'Upload-textAfter')
-    expect(textAfter.textContent).toEqual('hello world')
+    const wrapper = mount(<Upload textAfter={'hello world'} onChange={noop} />)
+    expect(wrapper.find('.Upload-textAfter').text()).toEqual('hello world')
   })
 
   it('should propagate disabled property to input type file and button', () => {
-    const instance = TestUtils.renderIntoDocument(
-      <Upload onChange={noop} disabled />
-    )
-    const fileInput = TestUtils.findRenderedDOMComponentWithClass(instance, 'Upload-fileInput')
-    expect(fileInput.disabled).toBe(true)
-    const button = TestUtils.findRenderedDOMComponentWithTag(instance, 'Button')
-    expect(button.disabled).toBe(true)
+    const wrapper = mount(<Upload disabled onChange={noop} />)
+    expect(wrapper.find('.Upload-fileInput').props().disabled).toBe(true)
+    expect(wrapper.find('.Button').props().disabled).toBe(true)
   })
 
   it('should ignore drag and drop events when disabled', () => {
-    const instance = TestUtils.renderIntoDocument(
-      <Upload onChange={noop} disabled />
-    )
-    const node = ReactDOM.findDOMNode(instance)
-    TestUtils.Simulate.dragEnter(node, {
+    const wrapper = mount(<Upload disabled onChange={noop} />)
+    wrapper.find('.Upload').simulate('dragenter', {
       dataTransfer: {
         files: [file]
       }
     })
-    expect(node.className).toContain('is-disabled')
-    expect(node.className).not.toContain('is-over-dropzone')
+    expect(wrapper.find('.Upload').hasClass('is-disabled')).toBe(true)
+    expect(wrapper.find('.Upload').hasClass('is-over-dropzone')).toBe(false)
   })
 
   it('should propagate accept property to input type file', () => {
-    const instance = TestUtils.renderIntoDocument(
-      <Upload onChange={noop} accept='.jpg' />
-    )
-    const fileInput = TestUtils.findRenderedDOMComponentWithClass(instance, 'Upload-fileInput')
-    expect(fileInput.accept).toEqual('.jpg')
+    const wrapper = mount(<Upload onChange={noop} accept='.jpg' />)
+    expect(wrapper.find('.Upload-fileInput').props().accept).toEqual('.jpg')
   })
 
   it('should not change style for not accepted file types', () => {
-    const instance = TestUtils.renderIntoDocument(
-      <Upload onChange={noop} accept='.md' />
-    )
-    const node = ReactDOM.findDOMNode(instance)
-    TestUtils.Simulate.dragEnter(node, {
+    const wrapper = mount(<Upload onChange={noop} accept='.md' />)
+    wrapper.find('.Upload').simulate('dragenter', {
       dataTransfer: {
         files: [{
           name: 'image.png',
@@ -78,15 +60,12 @@ describe('Upload', () => {
         }]
       }
     })
-    expect(node.className).not.toContain('is-over-dropzone')
+    expect(wrapper.find('.Upload').hasClass('is-over-dropzone')).toBe(false)
   })
 
   it('should change style for accepted file types', () => {
-    const instance = TestUtils.renderIntoDocument(
-      <Upload onChange={noop} accept='.png' />
-    )
-    const node = ReactDOM.findDOMNode(instance)
-    TestUtils.Simulate.dragEnter(node, {
+    const wrapper = mount(<Upload onChange={noop} accept='.png' />)
+    wrapper.find('.Upload').simulate('dragenter', {
       dataTransfer: {
         files: [{
           name: 'image.png',
@@ -94,103 +73,94 @@ describe('Upload', () => {
         }]
       }
     })
-    expect(node.className).toContain('is-over-dropzone')
+    expect(wrapper.find('.Upload').hasClass('is-over-dropzone')).toBe(true)
   })
 
   it('should toggle "is-over-window" css class on document dragenter and dragleave', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const node = ReactDOM.findDOMNode(instance)
+    const wrapper = mount(<Upload onChange={noop} />)
     document.dispatchEvent(new Event('dragenter'))
-    expect(node.className).toContain('is-over-window')
+    expect(wrapper.find('.Upload').hasClass('is-over-window')).toBe(true)
     document.dispatchEvent(new Event('dragleave'))
-    expect(node.className).not.toContain('is-over-window')
+    expect(wrapper.find('.Upload').hasClass('is-over-window')).toBe(false)
   })
 
   it('should toggle "is-over-dropzone" css class on dropzone dragenter and dragleave', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const node = ReactDOM.findDOMNode(instance)
-    TestUtils.Simulate.dragEnter(node)
-    expect(node.className).toContain('is-over-dropzone')
-    TestUtils.Simulate.dragLeave(node)
-    expect(node.className).not.toContain('is-over-dropzone')
+    const wrapper = mount(<Upload onChange={noop} />)
+    wrapper.find('.Upload').simulate('dragenter')
+    expect(wrapper.find('.Upload').hasClass('is-over-dropzone')).toBe(true)
+    wrapper.find('.Upload').simulate('dragleave')
+    expect(wrapper.find('.Upload').hasClass('is-over-dropzone')).toBe(false)
   })
 
   it('should remove dragenter event listener when component is unmounted', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
+    const wrapper = mount(<Upload onChange={noop} />)
+    const instance = wrapper.find('.Upload')
     // simulate unmount by calling method directly
-    instance.componentWillUnmount()
+    wrapper.unmount()
     // now simulate dragenter event and make sure component does not react anymore
-    const node = ReactDOM.findDOMNode(instance)
     document.dispatchEvent(new Event('dragenter'))
-    expect(node.className).not.toContain('is-over-window')
+    expect(instance.hasClass('is-over-window')).toBe(false)
   })
 
   it('should remove dragleave event listener when component is unmounted', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const node = ReactDOM.findDOMNode(instance)
+    const wrapper = mount(<Upload onChange={noop} />)
+    const instance = wrapper.find('.Upload')
     document.dispatchEvent(new Event('dragenter'))
     // make sure component has correct class
-    expect(node.className).toContain('is-over-window')
+    expect(instance.hasClass('is-over-window')).toBe(true)
     // now simulate unmount
-    instance.componentWillUnmount()
+    wrapper.unmount()
     // now simulate dragleave event and make sure component does not loose its class
     document.dispatchEvent(new Event('dragleave'))
-    expect(node.className).toContain('is-over-window')
+    expect(instance.hasClass('is-over-window')).toBe(true)
   })
 
   it('should propagate button click event to input element', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const node = ReactDOM.findDOMNode(instance)
+    const wrapper = mount(<Upload onChange={noop} />)
     // attach dummy click event handler to input dom element
-    const fileInput = TestUtils.findRenderedDOMComponentWithClass(instance, 'Upload-fileInput')
     let clicked = false
-    fileInput.addEventListener('click', () => { clicked = true })
+    wrapper.find('.Upload-fileInput').node.addEventListener('click', () => { clicked = true })
     // simulate click event on top level node which is the div
-    TestUtils.Simulate.click(node)
+    wrapper.find('.Upload-fileInput').simulate('click')
     expect(clicked).toBe(false)
-    const button = TestUtils.findRenderedDOMComponentWithTag(instance, 'Button')
-    TestUtils.Simulate.click(button)
+    wrapper.find('.Button').simulate('click')
     expect(clicked).toBe(true)
   })
 
   it('should not allow dropping multiple files in firefox', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const node = ReactDOM.findDOMNode(instance)
-    TestUtils.Simulate.dragEnter(node, {
+    const wrapper = mount(<Upload onChange={noop} />)
+    wrapper.find('.Upload').simulate('dragenter', {
       dataTransfer: {
         mozItemCount: 2
       }
     })
-    expect(node.className).not.toContain('is-over-dropzone')
+    expect(wrapper.find('.Upload').hasClass('is-over-dropzone')).toBe(false)
   })
 
   it('should not allow dropping multiple files in chrome', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const node = ReactDOM.findDOMNode(instance)
-    TestUtils.Simulate.dragEnter(node, {
+    const wrapper = mount(<Upload onChange={noop} />)
+    wrapper.find('.Upload').simulate('dragenter', {
       dataTransfer: {
         items: [file, file]
       }
     })
-    expect(node.className).not.toContain('is-over-dropzone')
+    expect(wrapper.find('.Upload').hasClass('is-over-dropzone')).toBe(false)
   })
 
   it('should allow dropping a single file', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const node = ReactDOM.findDOMNode(instance)
-    TestUtils.Simulate.dragEnter(node, {
+    const wrapper = mount(<Upload onChange={noop} />)
+    wrapper.find('.Upload').simulate('dragenter', {
       dataTransfer: {
         items: [file]
       }
     })
-    expect(node.className).toContain('is-over-dropzone')
+    expect(wrapper.find('.Upload').hasClass('is-over-dropzone')).toBe(true)
   })
 
   it('should handle dragover event on dropzone', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const node = ReactDOM.findDOMNode(instance)
+    const wrapper = mount(<Upload onChange={noop} />)
     let preventDefaultCalled = false
-    TestUtils.Simulate.dragOver(node, {
+    wrapper.find('.Upload').simulate('dragover', {
       preventDefault: () => {
         preventDefaultCalled = true
       }
@@ -199,10 +169,9 @@ describe('Upload', () => {
   })
 
   it('should not allow dragging multiple files over dropzone in firefox', () => {
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={noop} />)
-    const node = ReactDOM.findDOMNode(instance)
+    const wrapper = mount(<Upload onChange={noop} />)
     let preventDefaultCalled = false
-    TestUtils.Simulate.dragOver(node, {
+    wrapper.find('.Upload').simulate('dragover', {
       preventDefault: () => {
         preventDefaultCalled = true
       },
@@ -218,9 +187,8 @@ describe('Upload', () => {
     const callback = (files) => {
       name = files[0].name
     }
-    const instance = TestUtils.renderIntoDocument(<Upload onChange={callback} />)
-    const node = ReactDOM.findDOMNode(instance)
-    TestUtils.Simulate.drop(node, {
+    const wrapper = mount(<Upload onChange={callback} />)
+    wrapper.find('.Upload').simulate('drop', {
       dataTransfer: {
         files: [file]
       }
