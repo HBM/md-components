1511f7018f3443d90643397a2fe9813bff3524d5
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-08-09 08:53:28 +0200

    header: show box shadow only on scroll (fix #7)

--- components/header/index.js
+++ components/header/index.js
@@ -1,40 +1,61 @@
 
 import React from 'react'
+import classnames from 'classnames'
 import Icon from '../icon/'
 
 /**
  * Header component
  */
-var Header = ({children, title, subtitle}) => (
-  <header>
-    <div className='Header'>
-      <div className='Header-block'>
-        <span className='Header-title'>
-          {title}
-        </span>
-        {subtitle &&
-          <span className='Header-subtitle'>
-            <Icon.ChevronRight className='Header-chevron' />
-            {subtitle}
-          </span>
-        }
-      </div>
-      <div className='Header-block'>
-        {children}
-      </div>
-    </div>
-  </header>
-)
+export default class Header extends React.Component {
 
-/**
- * Property types
- */
-Header.propTypes = {
-  title: React.PropTypes.string,
-  subtitle: React.PropTypes.string
+  state = {
+    hasShadow: false
+  }
+
+  static propTypes = {
+    title: React.PropTypes.string,
+    subtitle: React.PropTypes.string
+  }
+
+  componentDidMount () {
+    window.addEventListener('scroll', this.onScroll)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.onScroll)
+  }
+
+  onScroll = () => {
+    this.setState({
+      hasShadow: window.scrollY > 0
+    })
+  }
+
+  render () {
+    const {title, subtitle, children} = this.props
+    const klass = classnames({
+      'Header--shadow': this.state.hasShadow
+    })
+    return (
+      <header className={klass}>
+        <div className='Header'>
+          <div className='Header-block'>
+            <span className='Header-title'>
+              {title}
+            </span>
+            {subtitle &&
+              <span className='Header-subtitle'>
+                <Icon.ChevronRight className='Header-chevron' />
+                {subtitle}
+              </span>
+            }
+          </div>
+          <div className='Header-block'>
+            {children}
+          </div>
+        </div>
+      </header>
+    )
+  }
+
 }
-
-/**
- * Export component
- */
-export default Header
