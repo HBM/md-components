fb5471e8b7e63c71f8ba7ee4de4feddc4114aade
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-09-06 15:02:44 +0200

    fix progress tests after using progress namespace

--- components/progress/test/test.js
+++ components/progress/test/test.js
@@ -3,12 +3,12 @@
 import assert from 'assert'
 import React from 'react'
 import {mount} from 'enzyme'
-import {Linear, Circular} from '../'
+import Progress from '../'
 
 describe('Progress', () => {
   describe('Linear', () => {
     it('should work', (done) => {
-      const wrapper = mount(<Linear />)
+      const wrapper = mount(<Progress.Linear />)
       // use setTimeout to make component is rendered
       window.setTimeout(() => {
         assert.equal(wrapper.find('.Progress-linear').length, 1)
@@ -17,7 +17,7 @@ describe('Progress', () => {
     })
 
     it('should render a progress bar with given percentage', (done) => {
-      const wrapper = mount(<Linear percentage={50} />)
+      const wrapper = mount(<Progress.Linear percentage={50} />)
       window.setTimeout(() => {
         assert.equal(wrapper.find('.Progress-linear-inner').node.style.width, '50%')
         done()
@@ -25,7 +25,7 @@ describe('Progress', () => {
     })
 
     it('should remove itself from the DOM when done', (done) => {
-      const wrapper = mount(<Linear />)
+      const wrapper = mount(<Progress.Linear />)
       window.setTimeout(() => {
         assert.equal(wrapper.find('.Progress-linear').length, 1)
         // let it be done
@@ -41,7 +41,7 @@ describe('Progress', () => {
 
     // make sure else path is taken for test coverage
     it('should not remove itself from the DOM when below 100 percent', (done) => {
-      const wrapper = mount(<Linear />)
+      const wrapper = mount(<Progress.Linear />)
       window.setTimeout(() => {
         assert.equal(wrapper.find('.Progress-linear').length, 1)
         wrapper.setProps({
@@ -58,12 +58,12 @@ describe('Progress', () => {
 
   describe('Circular', () => {
     it('should work', () => {
-      const wrapper = mount(<Circular />)
+      const wrapper = mount(<Progress.Circular />)
       assert.equal(wrapper.find('.Progress-circular').length, 1)
     })
 
     it('should render circular progress with given percentage', () => {
-      const wrapper = mount(<Circular percentage={50} />)
+      const wrapper = mount(<Progress.Circular percentage={50} />)
       const inner = wrapper.find('.Progress-circular-path')
       assert.equal(parseFloat(inner.node.style.strokeDasharray), 2 * Math.PI * 30)
       assert.equal(inner.node.style.strokeDashoffset, Math.PI * 30)
