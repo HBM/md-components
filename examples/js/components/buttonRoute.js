de19d85b64378bc668e79227d8c2d99e8e5b3e7c
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-09-07 10:07:41 +0200

    start working on live editing components

--- examples/js/components/buttonRoute.js
+++ examples/js/components/buttonRoute.js
@@ -1,45 +1,64 @@
 
 import React from 'react'
+import Playground from 'component-playground'
 import {Button} from '../../../'
 
-export default class ButtonRoute extends React.Component {
+const defaultButton =
+`<button type='button'>
+  Default
+</button>`
 
-  onClick = (event) => {
-    console.log(event)
-  }
+const buttonComponent =
+`<Button onClick={(event) => {console.log(event)}}>
+  Button
+</Button>`
+
+const buttonDisabled =
+`<Button onClick={() => {}} disabled>
+  Disabled
+</Button>`
+
+const buttonRaised =
+`<Button onClick={this.onClick} raised>
+  Raised
+</Button>`
+
+export default class ButtonRoute extends React.Component {
 
   render () {
     return (
       <div>
         <section>
           <h2>Default button</h2>
-          <button type='button'>
-            Button
-          </button>
+          <Playground
+            codeText={defaultButton}
+            scope={{React, Button}}
+          />
         </section>
         <section>
-          <h2>Button</h2>
-          <Button onClick={this.onClick}>
-            Button
-          </Button>
+          <h2>Button Component</h2>
+          <Playground
+            docClass={Button}
+            propDescriptionMap={{
+              type: 'submit || reset || button'
+            }}
+            codeText={buttonComponent}
+            scope={{React, Button}}
+          />
         </section>
         <section>
           <h2>Disabled Button</h2>
-          <Button onClick={this.onClick} disabled>
-            Button
-          </Button>
+          <Playground
+            codeText={buttonDisabled}
+            scope={{React, Button}}
+          />
         </section>
         <section>
-          <h2>Raised button</h2>
-          <Button onClick={this.onClick} raised>
-            Button
-          </Button>
-        </section>
-        <section>
-          <h2>Raised disabled button</h2>
-          <Button onClick={this.onClick} raised disabled>
-            Button
-          </Button>
+          <h2>Raised Button</h2>
+          <Playground
+            codeText={buttonRaised}
+            scope={{React, Button}}
+          />
         </section>
       </div>
     )
