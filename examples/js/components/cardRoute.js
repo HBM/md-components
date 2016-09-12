a35d7cead934af104334d8f46657a3739b62d684
Fahri Soenmez <fahri.soenmez@hbm.com>
2016-09-12 10:11:08 +0200

    move code in playground

--- examples/js/components/cardRoute.js
+++ examples/js/components/cardRoute.js
@@ -1,50 +1,61 @@
 
 import React from 'react'
+import Playground from 'component-playground'
 import {Button, Card, Title, Text, Actions} from '../../../'
 
-export default class CardRoute extends React.Component {
+const cardWithTitle =
+`<Card>
+  <Title>
+    Title
+  </Title>
+  <Text>
+    Greyhound divisively hello coldly wonderfully marginally far upon excluding.
+  </Text>
+  <Actions>
+    <Button onClick={(event) => {console.log(event)}}>
+      Cancel
+    </Button>
+    <Button onClick={(event) => {console.log(event)}}>
+      Submit
+    </Button>
+  </Actions>
+</Card>`
 
-  onClick = (event) => {
-    console.log(event)
-  }
+const cardWithoutTitle =
+`<Card>
+  <Text>
+    Greyhound divisively hello coldly wonderfully marginally far upon excluding.
+  </Text>
+  <Actions>
+    <Button onClick={(event) => {console.log(event)}}>
+      Cancel
+    </Button>
+    <Button onClick={(event) => {console.log(event)}}>
+      Submit
+    </Button>
+  </Actions>
+</Card>`
+
+export default class CardRoute extends React.Component {
 
   render () {
     return (
       <div>
         <section>
           <h2>Card</h2>
-          <Card>
-            <Title>
-              Title
-            </Title>
-            <Text>
-              Greyhound divisively hello coldly wonderfully marginally far upon excluding.
-            </Text>
-            <Actions>
-              <Button onClick={this.onClick}>
-                Cancel
-              </Button>
-              <Button onClick={this.onClick}>
-                Submit
-              </Button>
-            </Actions>
-          </Card>
+          <Playground
+            docClass={Card}
+            codeText={cardWithTitle}
+            scope={{React, Button, Card, Title, Text, Actions}}
+          />
         </section>
         <section>
           <h2>Card without title</h2>
-          <Card>
-            <Text>
-              Greyhound divisively hello coldly wonderfully marginally far upon excluding.
-            </Text>
-            <Actions>
-              <Button onClick={this.onClick}>
-                Cancel
-              </Button>
-              <Button onClick={this.onClick}>
-                Submit
-              </Button>
-            </Actions>
-          </Card>
+          <Playground
+            docClass={Card}
+            codeText={cardWithoutTitle}
+            scope={{React, Button, Card, Title, Text, Actions}}
+          />
         </section>
       </div>
     )
--- examples/js/components/checkboxRoute.js
+++ examples/js/components/checkboxRoute.js
@@ -1,9 +1,17 @@
 
 import React from 'react'
+import ReactDOM from 'react-dom'
+import Playground from 'component-playground'
 import {Checkbox} from '../../../'
 
-export default class CheckboxRoute extends React.Component {
+const _classDefault =
+`<label>
+  Label
+    <input type='checkbox' />
+</label>`
 
+const _classCheckbox =
+`class App extends React.Component {
   state = {
     checked: false
   }
@@ -17,20 +25,66 @@ export default class CheckboxRoute extends React.Component {
   render () {
     return (
       <div>
+        <Checkbox onChange={this.onChange} checked={this.state.checked} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, mountNode)`
+
+const _classDisableCheckbox =
+`class App extends React.Component {
+  state = {
+    checked: false
+  }
+
+  onChange = (event) => {
+    this.setState({
+      checked: !this.state.checked
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Checkbox onChange={this.onChange} checked={this.state.checked} disabled />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, mountNode)`
+
+export default class CheckboxRoute extends React.Component {
+
+  render () {
+    return (
+      <div>
         <section>
           <h2>Default checkbox</h2>
-          <label>
-            Label
-            <input type='checkbox' />
-          </label>
+          <Playground
+            codeText={_classDefault}
+            scope={{React}}
+          />
         </section>
         <section>
           <h2>Checkbox</h2>
-          <Checkbox onChange={this.onChange} checked={this.state.checked} />
+          <Playground
+            docClass={Checkbox}
+            codeText={_classCheckbox}
+            noRender={false}
+            scope={{React, ReactDOM, Checkbox}}
+          />
         </section>
         <section>
           <h2>Disabled checkbox</h2>
-          <Checkbox onChange={this.onChange} checked={this.state.checked} disabled />
+          <Playground
+            docClass={Checkbox}
+            codeText={_classDisableCheckbox}
+            noRender={false}
+            scope={{React, ReactDOM, Checkbox}}
+          />
         </section>
       </div>
     )
