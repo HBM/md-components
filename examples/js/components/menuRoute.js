4367818e08b270d677d5b48b4d6d49d722623982
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-09-12 12:58:12 +0200

    add playground to menu example

--- examples/js/components/menuRoute.js
+++ examples/js/components/menuRoute.js
@@ -1,57 +1,29 @@
 
 import React from 'react'
+import ReactDOM from 'react-dom'
+import Playground from 'component-playground'
 import {Menu, Divider} from '../../../'
 import Icon from '../../../components/icon/'
 
-export default class MenuRoute extends React.Component {
+const menuTopRight =
+`class App extends React.Component {
 
   state = {
-    rightTop: false,
-    leftTop: false,
-    rightBottom: false,
-    leftBottom: false
+    visible: false
   }
 
   onClick = (item) => {
-    console.log(item)
+    this.setState({
+      visible: true
+    })
+    document.addEventListener('click', this.hide)
   }
 
   hide = () => {
     this.setState({
-      rightTop: false,
-      leftTop: false,
-      rightBottom: false,
-      leftBottom: false
+      visible: false
     })
     document.removeEventListener('click', this.hide)
-  }
-
-  showRightTop = () => {
-    this.setState({
-      rightTop: true
-    })
-    document.addEventListener('click', this.hide)
-  }
-
-  showLeftTop = () => {
-    this.setState({
-      leftTop: true
-    })
-    document.addEventListener('click', this.hide)
-  }
-
-  showRightBottom = () => {
-    this.setState({
-      rightBottom: true
-    })
-    document.addEventListener('click', this.hide)
-  }
-
-  showLeftBottom = () => {
-    this.setState({
-      leftBottom: true
-    })
-    document.addEventListener('click', this.hide)
   }
 
   render () {
@@ -63,78 +35,205 @@ export default class MenuRoute extends React.Component {
       {content: 'long string without linebreak'}
     ]
     return (
+      <Menu
+        transformX='right'
+        transformY='top'
+        onClick={this.onClick}
+        items={items}
+        visible={this.state.visible}
+      >
+        <Icon.Button onClick={this.onClick}>
+          <Icon.MoreVert />
+        </Icon.Button>
+      </Menu>
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, mountNode)`
+
+const menuTopLeft =
+`class App extends React.Component {
+
+  state = {
+    visible: false
+  }
+
+  onClick = (item) => {
+    this.setState({
+      visible: true
+    })
+    document.addEventListener('click', this.hide)
+  }
+
+  hide = () => {
+    this.setState({
+      visible: false
+    })
+    document.removeEventListener('click', this.hide)
+  }
+
+  render () {
+    let items = [
+      {content: 'one', id: 'one'},
+      {content: 'two', icon: <Icon.Done />, id: 'two'},
+      Divider,
+      {content: 'three'},
+      {content: 'long string without linebreak'}
+    ]
+    return (
+      <Menu
+        transformX='left'
+        transformY='top'
+        onClick={this.onClick}
+        items={items}
+        visible={this.state.visible}
+      >
+        <Icon.Button onClick={this.onClick}>
+          <Icon.MoreVert />
+        </Icon.Button>
+      </Menu>
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, mountNode)`
+
+const menuBottomRight =
+`class App extends React.Component {
+
+  state = {
+    visible: false
+  }
+
+  onClick = (item) => {
+    this.setState({
+      visible: true
+    })
+    document.addEventListener('click', this.hide)
+  }
+
+  hide = () => {
+    this.setState({
+      visible: false
+    })
+    document.removeEventListener('click', this.hide)
+  }
+
+  render () {
+    let items = [
+      {content: 'one', id: 'one'},
+      {content: 'two', icon: <Icon.Done />, id: 'two'},
+      Divider,
+      {content: 'three'},
+      {content: 'long string without linebreak'}
+    ]
+    return (
+      <Menu
+        transformX='right'
+        transformY='bottom'
+        onClick={this.onClick}
+        items={items}
+        visible={this.state.visible}
+      >
+        <Icon.Button onClick={this.onClick}>
+          <Icon.MoreVert />
+        </Icon.Button>
+      </Menu>
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, mountNode)`
+
+const menuBottomLeft =
+`class App extends React.Component {
+
+  state = {
+    visible: false
+  }
+
+  onClick = (item) => {
+    this.setState({
+      visible: true
+    })
+    document.addEventListener('click', this.hide)
+  }
+
+  hide = () => {
+    this.setState({
+      visible: false
+    })
+    document.removeEventListener('click', this.hide)
+  }
+
+  render () {
+    let items = [
+      {content: 'one', id: 'one'},
+      {content: 'two', icon: <Icon.Done />, id: 'two'},
+      Divider,
+      {content: 'three'},
+      {content: 'long string without linebreak'}
+    ]
+    return (
+      <Menu
+        transformX='left'
+        transformY='bottom'
+        onClick={this.onClick}
+        items={items}
+        visible={this.state.visible}
+      >
+        <Icon.Button onClick={this.onClick}>
+          <Icon.MoreVert />
+        </Icon.Button>
+      </Menu>
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, mountNode)`
+
+export default class MenuRoute extends React.Component {
+
+  render () {
+    return (
       <div>
         <section>
-          <h2>Menu</h2>
-          <ul>
-            <li>transformX='right'</li>
-            <li>transformY='top'</li>
-          </ul>
-          <Menu
-            transformX='right'
-            transformY='top'
-            onClick={this.onClick}
-            items={items}
-            visible={this.state.rightTop}
-          >
-            <Icon.Button onClick={this.showRightTop}>
-              <Icon.MoreVert />
-            </Icon.Button>
-          </Menu>
+          <h2>Menu - top, right</h2>
+          <Playground
+            docClass={Menu}
+            codeText={menuTopRight}
+            noRender={false}
+            scope={{React, ReactDOM, Menu, Icon, Divider}}
+          />
         </section>
         <section>
-          <h2>Menu</h2>
-          <ul>
-            <li>transformX='left'</li>
-            <li>transformY='top'</li>
-          </ul>
-          <Menu
-            transformX='left'
-            transformY='top'
-            onClick={this.onClick}
-            items={items}
-            visible={this.state.leftTop}
-          >
-            <Icon.Button onClick={this.showLeftTop}>
-              <Icon.MoreVert />
-            </Icon.Button>
-          </Menu>
+          <h2>Menu - top, left</h2>
+          <Playground
+            codeText={menuTopLeft}
+            noRender={false}
+            scope={{React, ReactDOM, Menu, Icon, Divider}}
+          />
         </section>
         <section>
-          <h2>Menu</h2>
-          <ul>
-            <li>transformX='right'</li>
-            <li>transformY='bottom'</li>
-          </ul>
-          <Menu
-            transformX='right'
-            transformY='bottom'
-            onClick={this.onClick}
-            items={items}
-            visible={this.state.rightBottom}
-          >
-            <Icon.Button onClick={this.showRightBottom}>
-              <Icon.MoreVert />
-            </Icon.Button>
-          </Menu>
+          <h2>Menu - bottom, right</h2>
+          <Playground
+            codeText={menuBottomRight}
+            noRender={false}
+            scope={{React, ReactDOM, Menu, Icon, Divider}}
+          />
         </section>
         <section>
-          <h2>Menu</h2>
-          <ul>
-            <li>transformX='left'</li>
-            <li>transformY='bottom'</li>
-          </ul>
-          <Menu
-            transformX='left'
-            transformY='bottom'
-            onClick={this.onClick}
-            items={items}
-            visible={this.state.leftBottom}
-          >
-            <Icon.Button onClick={this.showLeftBottom}>
-              <Icon.MoreVert />
-            </Icon.Button>
-          </Menu>
+          <h2>Menu - bottom, left</h2>
+          <Playground
+            codeText={menuBottomLeft}
+            noRender={false}
+            scope={{React, ReactDOM, Menu, Icon, Divider}}
+          />
         </section>
       </div>
     )
