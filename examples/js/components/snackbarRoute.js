633396b110f249cd45a5f4ef7f85cb65ffcc8125
Fahri Soenmez <fahri.soenmez@hbm.com>
2016-09-13 09:18:16 +0200

    add playground to Snackbar example

--- examples/js/components/snackbarRoute.js
+++ examples/js/components/snackbarRoute.js
@@ -1,6 +1,39 @@
 
 import React from 'react'
+import ReactDOM from 'react-dom'
+import Playground from 'component-playground'
 import {Snackbar, Button} from '../../../'
+
+const snackbarComponent =
+`class App extends React.Component {
+  state = {
+    visible: false
+  }
+
+  toggle = () => {
+    this.setState({
+      visible: !this.state.visible
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Button onClick={this.toggle}>
+          Toggle
+        </Button>
+        <Snackbar
+          text='Ethernet settings saved'
+          action='Undo'
+          onAction={this.toggle}
+          visible={this.state.visible}
+        />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, mountNode)`
 
 export default class SnackbarRoute extends React.Component {
 
@@ -19,16 +52,13 @@ export default class SnackbarRoute extends React.Component {
       <div>
         <section>
           <h2>Snackbar</h2>
-          <Button onClick={this.toggle}>
-            Toggle
-          </Button>
+          <Playground
+            docClass={Snackbar}
+            noRender={false}
+            codeText={snackbarComponent}
+            scope={{React, ReactDOM, Snackbar, Button}}
+          />
         </section>
-        <Snackbar
-          text='Ethernet settings saved'
-          action='Undo'
-          onAction={this.toggle}
-          visible={this.state.visible}
-        />
       </div>
     )
   }
