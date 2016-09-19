cf8fe51d28864119a1c9e0896fa90092b0c2576c
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-09-19 09:10:56 +0200

    fix github buttons on example page after rename

--- examples/js/components/homeRoute.js
+++ examples/js/components/homeRoute.js
@@ -7,21 +7,21 @@ export default class HomeRoute extends React.Component {
     return (
       <div>
         <iframe
-          src='https://ghbtns.com/github-btn.html?user=hbm&repo=react-components&type=star&count=true&size=large'
+          src='https://ghbtns.com/github-btn.html?user=hbm&repo=md-components&type=star&count=true&size=large'
           frameBorder='0'
           scrolling='0'
           width='160px'
           height='30px'
         />
         <iframe
-          src='https://ghbtns.com/github-btn.html?user=hbm&repo=react-components&type=watch&count=true&size=large&v=2'
+          src='https://ghbtns.com/github-btn.html?user=hbm&repo=md-components&type=watch&count=true&size=large&v=2'
           frameBorder='0'
           scrolling='0'
           width='160px'
           height='30px'
         />
         <iframe
-          src='https://ghbtns.com/github-btn.html?user=hbm&repo=react-components&type=fork&count=true&size=large'
+          src='https://ghbtns.com/github-btn.html?user=hbm&repo=md-components&type=fork&count=true&size=large'
           frameBorder='0'
           scrolling='0'
           width='158px'
