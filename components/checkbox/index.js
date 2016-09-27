936b2a2e21b384334b79ae4135c60907b9a61e3c
Gerhard Preuss <gelipp@gmail.com>
2016-09-27 17:27:32 +0200

    keep icons DRY

--- components/checkbox/index.js
+++ components/checkbox/index.js
@@ -1,36 +1,12 @@
 
 import React from 'react'
 import classnames from 'classnames'
-
-/**
- * Check box - ic_check_box_24px.svg
- */
-const IconCheckbox = () => (
-  <svg width='24' height='24' viewBox='0 0 24 24'>
-    <path
-      d='M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z'
-    />
-  </svg>
-)
-
-/**
- * Check box outline blank - ic_check_box_outline_blank_24px.svg
- */
-const IconCheckboxOutline = () => (
-  <svg width='24' height='24' viewBox='0 0 24 24'>
-    <path
-      d='M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'
-    />
-  </svg>
-)
+import {CheckBox, CheckBoxOutlineBlank} from '../icon'
 
 export default class Checkbox extends React.Component {
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      active: false
-    }
+  state = {
+    active: false
   }
 
   onCleanUp = (e) => {
@@ -62,8 +38,8 @@ export default class Checkbox extends React.Component {
         <div className='Checkbox-focus' />
         <div className='Checkbox-icon'>
           {this.props.checked || this.props.defaultChecked
-            ? <IconCheckbox />
-            : <IconCheckboxOutline />
+            ? <CheckBox />
+            : <CheckBoxOutlineBlank />
           }
         </div>
         <span className='Checkbox-label'>
