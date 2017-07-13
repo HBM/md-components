c4ed7655236213e3df6a933e7132372a4fa021a8
Gerhard Preuss <gelipp@gmail.com>
2017-07-13 11:26:10 +0200

    make tabs a pure component (fixes #346) (#347)

--- src/js/tabs/index.js
+++ src/js/tabs/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-class Tabs extends React.Component {
+class Tabs extends React.PureComponent {
   static propTypes = {
     tabs: PropTypes.array
   }
