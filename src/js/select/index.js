f021a5c2fd5b038327b0d864c9df0ce79c0f130e
Fahri <Fa-So@users.noreply.github.com>
2017-12-18 10:02:54 +0100

    select: alignment at the bottom edge (#408)

--- src/js/select/index.js
+++ src/js/select/index.js
@@ -325,7 +325,12 @@ export class List extends React.Component {
     // If the select panel is very bottom, tries to adjust the position 'top' so the panel can open fully on-screen.
     // If it still won't fit, sets the offset back to 0 to allow the fallback position to take over.
     const innerHeight = window.innerHeight
-    const listHeight = LIST_ITEM_HEIGHT * Math.min(MAX_LIST_LENGTH, options.length) + PADDING_TOP
+    let listHeight = LIST_ITEM_HEIGHT * Math.min(MAX_LIST_LENGTH, options.length)
+    if (this.props.isInsideTable) {
+      listHeight += PADDING_TOP
+    } else {
+      listHeight -= PADDING_TOP
+    }
     if (top < 0) {
       top = 0
     } else if (top + listHeight > innerHeight) {
